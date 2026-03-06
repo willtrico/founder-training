c1c67f89902edc256813982b6c15a1c600a74924
Will Cowen <willcowen94@gmail.com>
2026-03-05 16:30:00 -0800

    Day 2 - added cost of goods to calculator

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function calculate() {
   const spend = Number(document.getElementById("spend").value);
   const revenue = Number(document.getElementById("revenue").value);
+  const costOfGoods = Number(document.getElementById("costOfGoods").value)
 
-  if (!spend || !revenue) {
+  if (!spend || !revenue || !costOfGoods) {
     document.getElementById("result").innerText = 
-      "Please enter both spend and revenue.";
+      "Please enter spend, revenue and cost of goods.";
     return;
   }
 
-  const roi = ((revenue - spend) / spend) * 100;
+  const roi = ((revenue - spend - costOfGoods) / spend) * 100;
 
   document.getElementById("result").innerText =
     "ROI: " + roi.toFixed(2) + "%";
